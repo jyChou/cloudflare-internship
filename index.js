ead3a95f812448e456281d2bce950b5a8c017ae4
jyChou <a23064569@gmail.com>
2020-04-16 16:16:42 -0400

    add rewriter, error handling

--- index.js
+++ index.js
@@ -1,19 +1,8 @@
 class titleRewriter {
-  constructor() {
-    this.buffer = "";
-  }
   text(text) {
-    // concatenate all text until it reaches lastInTextNode
-    this.buffer += text.text;
+    // remove all text until it reaches lastInTextNode
     if (text.lastInTextNode) {
-      text.replace(
-        this.buffer.replace(
-          /Variant [12]+/,
-          "Cloudflare-internship-2020-fullstack"
-        )
-      );
-      // reset buffer
-      this.buffer = "";
+      text.replace("Cloudflare-internship-2020-fullstack");
     } else {
       // remove any intermediate text
       text.remove();
@@ -22,43 +11,24 @@ class titleRewriter {
 }
 
 class headingRewriter {
-  constructor() {
-    this.buffer = "";
-  }
   text(text) {
-    // concatenate text until it reaches lastInTextNode
-    this.buffer += text.text;
+    // remove all text until it reaches lastInTextNode
     if (text.lastInTextNode) {
-      text.replace(
-        this.buffer.replace(
-          /Variant [12]+/, 
-          "Chin-Yu Chou, MS@Cornell Tech"
-        )
-      );
-      // reset buffer
-      this.buffer = "";
+      text.replace("Chin-Yu Chou, MS@Cornell Tech");
     } else {
       // remove any intermediate text
       text.remove();
     }
   }
 }
+
 class descRewriter {
-  constructor() {
-    this.buffer = "";
-  }
   text(text) {
-    // concatenate all text until it reaches lastInTextNode
-    this.buffer += text.text;
+    // remove all text until it reaches lastInTextNode
     if (text.lastInTextNode) {
       text.replace(
-        this.buffer.replace(
-          /This is variant (one|two) of the take home project!/,
-          "Hi, I am Chin-Yu. I am interested in full stack developement and machine learning!"
-        )
+        "Hi, I am Chin-Yu. I am interested in full stack developement and machine learning!"
       );
-      // reset buffer
-      this.buffer = "";
     } else {
       // remove any intermediate text
       text.remove();
@@ -69,33 +39,21 @@ class descRewriter {
 class urlRewriter {
   constructor(targetAttribute) {
     this.targetAttribute = targetAttribute;
-    this.buffer = "";
   }
 
   element(element) {
     const attribute = element.getAttribute(this.targetAttribute);
     if (attribute) {
-      element.setAttribute(
-        this.targetAttribute,
-        attribute.replace(
-          "https://cloudflare.com", 
-          "https://github.com/jyChou"
-        )
-      );
+      element.setAttribute(this.targetAttribute, "https://github.com/jyChou");
     }
   }
 
   text(text) {
-    this.buffer += text.text;
+    // remove all text until it reaches lastInTextNode
     if (text.lastInTextNode) {
-      text.replace(
-        this.buffer.replace(
-          "Return to cloudflare.com",
-          "Go to Chin-Yu's github"
-        )
-      );
-      this.buffer = "";
+      text.replace("Go to Chin-Yu's github");
     } else {
+      // remove any intermediate text
       text.remove();
     }
   }
@@ -109,34 +67,36 @@ const rewriter = new HTMLRewriter()
 
 async function handleRequest(request) {
   // Get the urls of two variants
-  const { variants } = await fetch(
-    "https://cfw-takehome.developers.workers.dev/api/variants"
-  )
-    .then((response) => {
-      if (response.ok) {
-        return response.json();
-      } else {
-        throw new Error("API server error!");
-      }
-    })
-    .catch((error) => {
-      console.error(`Error:${error}`);
-    });
-
-  // Create response with correspoding paths
+  let variants = null;
+  try {
+    const response = await fetch(
+      "https://cfw-takehome.developers.workers.dev/api/variants"
+    );
+    if (response.ok) {
+      const data = await response.json();
+      variants = data["variants"];
+    } else {
+      throw new Error(response.status);
+    }
+  } catch (err) {
+    console.error(err);
+  }    
+  // if fetch fails, response with error status code
+  if(!variants){
+    return new Response("Fetch failed.");
+  }
+  // Create response of two variants
   const VARIANT_ONE = await fetch(variants[0]);
   const VARIANT_TWO = await fetch(variants[1]);
-
   // Get cookie
   const cookie = request.headers.get("cookie");
-
-  // Assign user to the presisting variant if cookie exists
+  // Assign user to the corresponding variant if cookie exists
   if (cookie && cookie.includes(`endpoint=1`)) {
     return rewriter.transform(VARIANT_ONE);
   } else if (cookie && cookie.includes(`endpoint=2`)) {
     return rewriter.transform(VARIANT_TWO);
   } else {
-    // create cookie for new user
+    // determine endpoint and generate cookie for new user
     const endpoint = Math.random() < 0.5 ? 1 : 2;
     let response;
     if (endpoint === 1) {
